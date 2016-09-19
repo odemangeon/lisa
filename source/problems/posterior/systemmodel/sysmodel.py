5ab39932ccce2ffc6cda522c07c8d92cbe5c405e
odemangeon <olivier.demangeon@lam.fr>
2016-09-19 12:34:18 +0200

    Update sysmodel.py. Redifine init function

--- source/problems/posterior/systemmodel/sysmodel.py
+++ source/problems/posterior/systemmodel/sysmodel.py
@@ -10,39 +10,76 @@ import numpy as np
 
 
 class SystemModel():
+    """
+    Sysmodel class which defines the model used.
 
-    number_star = 0
-    number_planet = 0
+    Provide function to model the observables: radial velocities, transit photometry (and SED)
+    """
+
+    nb_star = 0
+    nb_planet = 0
     dynamic = False
     ## List of available rv models
     _rv_models = ["ajplanet"]
+    rv_model = None
     ## List of available lc models
     _lc_models = ["batman", "pytransit"]
+    transit_model = None
 
-    def __init__(self, model):
-        # transit parameters
-        self.rp = 0.          # planet radius (in units of stellar radii)
-        self.ar = 0.           # semi-major axis (in units of stellar radii)
-        self.inc = 0.         # orbital inclination (in degrees)
+    def __init__(self, data_set,
+                 transit_model=None, rv_model=None, LD_model=None,
+                 nb_planet=1, nb_star=1):
+        """
+        Create SystemModel instance Object.
 
-        self.model = model
+        Defines the transit, rv models and SED models used. Defines the parametrisation.
+        So I think here we can either read the text file or fill some main information that will
+        produce a template and then we fill oit and create a function that will read it.
 
+        If we want to produce the a template, we need to give the transit model, the LD model used,
+        the rv model, the number of star and the number of planet and we need to have a
+        data_set instance.
+        """
+        # Define the transit model used
+        if transit_model in self._lc_models:
+            self.transit_model = transit_model
+        else:
+            raise ValueError("transit_model should be in ['batman', 'pytransit']")
+        # Define the rv model used
+        if rv_model in self._rv_models:
+            self.rv_model = rv_model
+        else:
+            raise ValueError("rv_model should be in ['ajplanet']")
+
+        # Define the number of planets in the system
+        if nb_planet >= 1:
+            self.nb_planet = nb_planet
+        # Define the number of stars in the system
+        if nb_star >= 1:
+            self.nb_star = nb_star
+
+        # Define the limb darkening model: I think we should have an argument to select the LD
+        # model here.
         if self.model == 'batman':
             # it can be changed later but I dont know how to make it 0 string
-            self.limb_dark = "quadratic" #  if  batman limb darkening model
+            self.limb_dark = "quadratic"  # if  batman limb darkening model
         else:
-            self.limb_dark = 2 # if pyttransit , do we want to give the option now ?
+            self.limb_dark = 2  # if pyttransit , do we want to give the option now ?
 
+        # transit parameters
+        self.rp = 0.          # planet radius (in units of stellar radii)
+        self.ar = 0.          # semi-major axis (in units of stellar radii)
+        self.inc = 0.         # orbital inclination (in degrees)
 
         '''
         this will depend on how many LC data sets ..? how to set it?
         '''
         self.jitter_lc = 0.
         self.u = [0., 0.]     # limb darkening coefficients
-        self.supersample = 0 # 21 supersampling factor needs to be defined for each light curve
-        self.exp_time = 0.5/24. # if supersampling is done we need to define this for each light curve
-        #we can also calculate it but sometimes if we miss data this will be wrong
-
+        self.supersample = 0  # 21 supersampling factor needs to be defined for each light curve
+        self.exp_time = 0.5 / 24.  # if supersampling is done we need to define this for each light
+        # curve
+        # we can also calculate it but sometimes if we miss data this will be wrong
 
         # rv parameters
         self.rvsys = 0.      # radial velocity systematic velocity
@@ -58,11 +95,14 @@ class SystemModel():
         '''
         This will depend if we if we are fiting individual transit times
         '''
-        self.t0 = 0.     # time of inferior conjunction
-        self.period = 0. # orbital period
+        self.t0 = 0.      # time of inferior conjunction
+        self.period = 0.  # orbital period
 
-
-
+    def create_filetemplate():
+        """
+        Create template file to be filed with the initial parameter values and prior functions
+        """
+        raise NotImplementedError
 
 
     def set_lc_model():
