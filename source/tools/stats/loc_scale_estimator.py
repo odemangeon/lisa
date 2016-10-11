fbdbb60c5296f6ca69f2af6db387f8014125b1cd
odemangeon <olivier.demangeon@lam.fr>
2016-10-11 10:27:36 +0200

    Tools: Add stats package with average and deviation estimators and a tentative mode.

---
+++ source/tools/stats/loc_scale_estimator.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Location and scale estimators module.
+
+@package dr_tools.stats.robust_moment
+@ingroup robust_moment
+
+@brief librairie of function to compute robust moments.
+@details Module which defines robust estimates of classical statistics location and scale.
+
+@file
+@author Olivier Demangeon
+@date July 06, 2016
+@version 5.2 06/07/2016 ODE # First released version
+@todo: Unit tests of mad and estimator_loc_scale
+"""
+
+from numpy import sort, median, where, sqrt, pi
+from numpy import sum as npsum, arange
+import statsmodels.api as sm
+
+## Estimator of location and scale with LeastSquares norm
+estimator_LeastSquares = sm.robust.Huber(norm=sm.robust.norms.LeastSquares())
+
+## Estimator of location and scale with HuberT norm
+estimator_HuberT = sm.robust.Huber(norm=sm.robust.norms.HuberT())
+
+## Estimator of location and scale with RamsayE norm
+estimator_RamsayE = sm.robust.Huber(norm=sm.robust.norms.RamsayE())
+
+## Estimator of location and scale with Andrew Wave norm
+estimator_AndrewWave = sm.robust.Huber(norm=sm.robust.norms.AndrewWave())
+
+## Estimator of location and scale with TrimmedMean norm
+estimator_TrimmedMean = sm.robust.Huber(norm=sm.robust.norms.TrimmedMean())
+
+## Estimator of location and scale with Hampel norm
+estimator_Hampel = sm.robust.Huber(norm=sm.robust.norms.Hampel())
+
+## Estimator of location and scale with TukeyBiweight norm
+estimator_TukeyBiweight = sm.robust.Huber(norm=sm.robust.norms.TukeyBiweight())
+
+## Dictionaries which associate the string of estimator and the estimator
+_estimators_dico = {"LeastSquares": estimator_LeastSquares,
+                    "HuberT": estimator_HuberT,
+                    "RamsayE": estimator_RamsayE,
+                    "AndrewWave": estimator_AndrewWave,
+                    "TrimmedMean": estimator_TrimmedMean,
+                    "Hampel": estimator_Hampel,
+                    "TukeyBiweight": estimator_TukeyBiweight}
+
+
+def estimator_loc_scale(data, norm=None, **kwargs):
+    """
+    Estimate location and scale.
+
+    Estimate location and scale and allows to select the norm to be used.
+
+    ----
+
+    @author Olivier Demangeon LAM
+
+    Arguments:
+        data : iterable,
+            data on which to compute the location and scale
+        norm : string, optional (default: TukeyBiweight),
+            Norm to use to compute the location and scale
+
+    Keyword arguments:
+        kwargs : keyword argument to be passed to the estimator
+
+    Returns:
+        results : tuple of 2 arrays, the first one give the location the second one the scale
+
+    Raises:
+        ValueError: Norm argument should be in ["LeastSquares", "HuberT", "RamsayE", "AndrewWave",
+                    "TrimmedMean", "Hampel", "TukeyBiweight"]
+    """
+    if norm in _estimators_dico.keys():
+        return _estimators_dico[norm](data)
+    else:
+        raise ValueError("Norm argument should be in {}".format(_estimators_dico.keys()))
+
+
+def mad(data, **kwargs):
+    """
+    Median absolute deviation.
+
+    ----
+
+    @author Olivier Demangeon LAM
+
+    Arguments:
+        data : iterable
+            data on which to compute the mad
+
+    Keyword arguments:
+        center : float, optional
+            center value to use
+        norm : norm to use to compute the center, optional
+            should be in statsmodels.api.robust.norms
+
+    Returns:
+        result : value of the mad
+    """
+    return sm.robust.mad(data, **kwargs)
+
+
+def rob_mom(data, center=None):
+    """
+    Calculate robust estimates of the central location and spread of a distribution.
+
+    It's based on Tuckey's biweight. For an uncontaminated distribution, these are identical to
+    the mean and the standard deviation.
+
+    The choices of the estimators are based on extensive simulations by Beers et al. (1990).
+    Use the biweight centre location to estimate the mean in as many cases as possible. Where it
+    fails, use the median instead.
+    For N < 10, use the gapper algorithm to estimate the spread.
+    For N > 10 use the median absolute deviation (MAD) as the initial estimate of the centre
+    location, then weight points using Tukey's Biweight to obtain an estimate of the spread.
+
+    References:
+    -----------
+    "Understanding Robust and Exploratory Data Analysis," by Hoaglin, Mosteller and Tukey,
+    John Wiley & Sons, 1983.
+    Beers, Flynn & Gebhardt, Astronomical Journal, vol. 100, 32, 1990.
+
+    Syntax:
+    -------
+    centre, spread = rob_mom(data, center=None)
+
+    Parameters:
+    -----------
+    data : ndarray
+        Vector with dataset for which the dispersion (and center) is to be calculated.
+    center : float, optional
+        If set, the spread is calculated w.r.t. zero rather than the central value of the vector.
+        If data is a vector of residuals, this should be set to zero.
+
+    Returns:
+    --------
+    centre : float
+        the central value of data. In case of failure, returns zero value.
+    spread : float
+        the spread of data. In case of failure, returns zero value.
+
+    Revision history"
+    -----------------
+    Based on the IDL routines ROBUST_SIGMA and MED by H. Freudenreich and the
+    Fortran 77 routine ROBUST.FOR by R.H. den Hartog, Leiden Observatory, 3/92.
+
+    Examples:
+    ---------
+        >>> data = np.random.rand(3,4)
+        >>> centre, spread = rob_mom(data)
+    """
+    # Set some useful values
+    eps = 1.0E-20
+    n = data.size
+
+    # xi = np.where(zero != 0) # Non
+    # zi = np.array(xi) #
+    # test = np.any(zi)
+
+    # If center is provided, take it as cen, otherwise use the median value of the data.
+    if center is None:
+        cen = median(data)
+    else:
+        cen = center
+
+    # Obtain the median absolute deviation about the median, or what it specified as the center
+    # of the distribution.
+    res = data - cen
+    mad = median(abs(res)) / 0.6745
+
+    # COMPUTE Centre
+    # Initialize centre (return) as cen
+    centre = cen
+
+    # If the MAD=0, try the mean absolute deviation, and if that failes, the median itself.
+    if mad <= eps:
+        mad = npsum(abs(res)) / n / 0.80
+
+    if mad > eps:
+        # Now the biweighted location estimator:
+        u = res / 6.0 / mad
+        indices = where(abs(u) < 1.0)
+        cnt = indices[0].size
+        if cnt > 3:
+            u2 = (1.0 - u[indices]**2)**2
+            centre = cen + npsum(res[indices] * u2) / npsum(u2)
+
+    # COMPUTE Spread
+    if n > 10:
+        # In the case of enough datapoints, apply Tukey's biweight estimator
+        spread = 0.
+        if mad > eps:
+            # The biweight scale estimator:
+            u = res / 9.0 / mad
+            indices = where(abs(u) < 1.0)
+            cnt = indices[0].size
+            if cnt >= 3:
+                u2 = u[indices]**2
+                u1 = 1.0 - u2
+                spread = (sqrt(n * npsum((u1**4) * res[indices]**2)) /
+                          abs(npsum(u1 * (1.0 - 5.0 * u2))))
+    else:
+        # In the limit of small numbers, use the gapper algorithm to estimate the spread.
+        # In the case of N=1, 2 provide a very rough estimate.
+        if n == 1:
+            spread = cen  # "Poissonian assumption"
+        if n == 2:
+            spread = 0.5 * abs(data.flatten()[1] - data.flatten()[0])
+        if n >= 3:
+            x = sort(data)
+            g = abs(x[1:n - 1] - x[0:n - 2])
+            w = arange(n - 1, dtype=float) + 1.0
+            w = w * (n - w)
+            spread = sqrt(pi) / (n * (n - 1)) * npsum(w * g)
+
+    return centre, spread
---
+++ source/tools/stats/mode.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Mode module.
+
+Module which defines the mode statistics functions
+
+@package dr_tools.stats.mode
+@ingroup mode
+
+@brief librairie of function to compute the mode of a distribution
+
+@file
+@author Olivier Demangeon
+@date July 06, 2016
+@version 5.2 06/07/2016 ODE # First released version
+@todo This module is way to make the statistical_background_estimate function in the background
+    estimate module reusable for other purpose. The idea is to remove the statistical_... functions
+    from the background estimate module and just call this module in the uniform_bkg_estimate
+    function. For that the interfaces of those function needs to be homogenized and the code made
+    more general. Work in progress.
+"""
+
+from numpy import sqrt, percentile, histogram, median, where, zeros_like, logical_and, mean
+
+
+def trimmode(data, bins=None, H_outliers=1, L_outliers=2, debug=False):
+    """
+    Estimated the mode of trimmed data set.
+
+    Arguments:
+    ----------
+    data : ndarray
+        Data sets
+    bins : int or None, optional (None)
+        Number of bins for the histogram
+    H_outliers : float, optional (2)
+        percentage of high value outliers in data
+    L_outliers : float, optional (1)
+        percentage of low value outliers in data
+    debug : Boolean, optional (False)
+        If true, output some debugging informations
+
+    Returns:
+    --------
+    mode : float
+        mode of the underlying distribution behind data
+
+    Raises:
+    -------
+    None
+    """
+    data_f = data.flatten()
+    nb_pix = data.size
+    if bins is None:
+        bins = "sqrt"
+    vmax = percentile(data_f, 100 - H_outliers)
+    vmin = percentile(data_f, L_outliers)
+    n, x_bins = histogram(data_f, bins=bins, range=(vmin, vmax))
+    if debug:
+        print("Median number of pixel per bin in the histogram : {}".format(median(n)))
+    idx = where(n == n.max())[0][0]
+    v_bkg_min = x_bins[idx]
+    v_bkg_max = x_bins[idx + 1]
+    if debug:
+        print("Max value of the mode bin : {} corresponding to {} % of High - low percentile"
+              "".format(v_bkg_max, (v_bkg_max - vmin) / (vmax - vmin) * 100))
+        print("Min value of the mode bin : {} corresponding to {} % of High - low percentile"
+              "".format(v_bkg_min, (v_bkg_min - vmin) / (vmax - vmin) * 100))
+    mask_bkg_stat = zeros_like(data)
+    mask_bkg_stat[logical_and(data > v_bkg_min, data < v_bkg_max)] = 1
+    result = median(data[logical_and(data > v_bkg_min, data < v_bkg_max)])
+    # mean(data[logical_and(data>v_bkg_min, data<v_bkg_max)])
+    if debug:
+        mean_mode = mean(data[logical_and(data > v_bkg_min, data < v_bkg_max)])
+        median_mode = median(data[logical_and(data > v_bkg_min, data < v_bkg_max)])
+        print("Number of pixels used to compute the backgroound level : {0} corresponding to {1} % "
+              "of the image".format(mask_bkg_stat.sum(), mask_bkg_stat.sum()/float(nb_pix)*100))
+        print("Mean value of the mode bin : {0} corresponding to {1} % of the max - min values"
+              "".format(mean_mode, (mean_mode-vmin)/(vmax-vmin)*100))
+        print("Median value of the mode bin : {0} corresponding to {1} % of the max - min values"
+              "".format(median_mode, (median_mode-vmin)/(vmax-vmin)*100))
+        print("Value of the backrgound estimate : {0} corresponding to {1} % of the max - min "
+              "values".format(result, (result-vmin)/(vmax-vmin)*100))
+    if debug:
+        fig = figure()
+        axe1 = fig.add_subplot(3, 3, 1)
+        imshow_colorbar(data, ax=axe1, norm=LogNorm())
+        axe1.set_title("Input image\n(index = %d)".format(debug_info['index']), size='small')
+        axe2 = fig.add_subplot(3, 3, 3)
+        imshow_colorbar(mask_bkg_stat, ax=axe2)
+        axe2.set_title("Background + stray-light pixels map\n(index = %d)"
+                       "".format(debug_info['index']),
+                       size='small')
+        axe3 = fig.add_subplot(3, 3, 2)
+        axe3.hist(data.flatten(), bins=sqrt(nb_pix)/2., cumulative=False,
+                  normed=False, log=True, histtype='step')
+        axe3.set_title("Histogram of the image\n(index={})".format(debug_info['index']),
+                       size='small')
+        axe3.set_ylabel("Number of pixels", size='small')
+        axe3.set_xlabel("Amplitude [ADU]", size='small')
+        ymin, ymax = axe3.get_ylim()
+        axe3.vlines(vmin, ymin, ymax, colors='g')
+        axe3.vlines(vmax, ymin, ymax, colors='r')
+        axe3.text(vmin-100, ymax, "Lth percentile", color='g', horizontalalignment='right',
+                  verticalalignment='top', size='small')
+        axe3.text(vmax+100, ymax, "Hth percentile", color='r', horizontalalignment='left',
+                  verticalalignment='top', size='small')
+        axe4 = fig.add_subplot(3, 3, 4)
+        axe4.hist(data.flatten(), bins=bins, cumulative=False, normed=False, log=True,
+                  histtype='step', range=(vmin, vmax))
+        axe4.set_xlim(xmin=vmin-(vmax-vmin)*5./100, xmax=vmax+(vmax-vmin)*5./100)
+        axe4.set_title("Histogram of the image between the\nH and L percentiles (index={})"
+                       "".format(debug_info['index']), size='small')
+        axe4.set_ylabel("Number of pixels", size='small')
+        axe4.set_xlabel("Amplitude [ADU]", size='small')
+        axe4.vlines(vmin, ymin, ymax, colors='g')
+        axe4.vlines(vmax, ymin, ymax, colors='r')
+        axe4.text(vmin+10, ymax, "Lth percentile", color='g', horizontalalignment='left',
+                  verticalalignment='top', size='small')
+        axe4.text(vmax-10, ymax, "Hth percentile", color='r', horizontalalignment='right',
+                  verticalalignment='top', size='small')
+        axe4.text((vmin+vmax)/2., (ymin+ymax)/2., "Background + Stray-light pixels",
+                  horizontalalignment='center', verticalalignment='center', size='small')
+        axe5 = fig.add_subplot(3, 3, 5)
+        imshow_colorbar(masked_array(data=data, mask=mask_bkg_stat), ax=axe5)
+        axe5.set_title("Non background + stray-light pixels\n(index = %d)"
+                       "".format(debug_info['index']),
+                       size='small')
+        axe6 = fig.add_subplot(3, 3, 6)
+        imshow_colorbar(masked_array(data=data, mask=logical_not(mask_bkg_stat)), ax=axe6)
+        axe6.set_title("Background + stray-light pixels\n(index = %d)"
+                       "".format(debug_info['index']), size='small')
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        bottom_spacing = 0.2
+        left_right_spacing = 0.05
+        fig.text(0.5, bottom_spacing,
+                 "Number of bins in the histogram : {0}\n"
+                 "(If uniform) Number of pixel per bin : {1}\n"
+                 "Max value in the image : {2}\n"
+                 "Min value in the image: {3}\n"
+                 "High percentile - min value = {4} % of the max - min values\n"
+                 "Low percentile - min value = {5} % of the max - min values\n"
+                 "Median number of pixel per bin in the histogram : {6}\n"
+                 "Max value of the mode bin : {7} corresponding to {8} % of High - low percentile\n"
+                 "Min value of the mode bin : {9} corresponding to {10} % of High -"
+                 " low percentile\n"
+                 "Number of pixels used to compute the backgroound level : {11} corresponding to "
+                 "{12} % of the image\n"
+                 "Mean value of the mode bin : {13} corresponding to {14} % of the max -"
+                 " min values\n"
+                 "Median value of the mode bin : {15} corresponding to {16} % of the max - "
+                 "min values\n"
+                 "Value of the backrgound estimate : {17} corresponding to {18} % of the max "
+                 "- min values"
+                 "".format(bins,
+                           nb_pix/float(bins),
+                           max_data,
+                           min_data,
+                           (vmax-min_data)/(max_data-min_data)*100,
+                           (vmin-min_data)/(max_data-min_data)*100,
+                           median(n),
+                           v_bkg_max, (v_bkg_max-vmin)/(vmax-vmin)*100,
+                           v_bkg_min, (v_bkg_min-vmin)/(vmax-vmin)*100,
+                           mask_bkg_stat.sum(), mask_bkg_stat.sum()/float(nb_pix)*100,
+                           mean_mode, (mean_mode-vmin)/(vmax-vmin)*100,
+                           median_mode, (median_mode-vmin)/(vmax-vmin)*100,
+                           result, (result-vmin)/(vmax-vmin)*100),
+                 size='small',
+                 bbox=props,
+                 va='center',
+                 ha='center',
+                 linespacing=0.6)
+        fig.subplots_adjust(top=0.95, bottom=bottom_spacing, left=left_right_spacing,
+                            right=1-left_right_spacing, wspace=0.2, hspace=0.4)
