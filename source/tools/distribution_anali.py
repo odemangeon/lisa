ea94870faa8d1ea832026f9e9eaebd01d2af2599
susana98 <susana.barros@astro.up.pt>
2016-12-14 16:46:07 +0000

    add code to compute the percentiles of distribution

---
+++ source/tools/distribution_anali.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Calculate statistics on the distributions
+
+@package tools
+@ingroup getconfi
+
+@brief calculate confidence limits for distributions
+@details calculates 1 sigma , 2 sigma , 3 sigma percentiles for distributions
+if centre is given it is used for the limits otherwise the rob_mon is used and returned
+
+
+@file
+@author Susana Barros
+@date  Decembre 14, 2016
+@version 1.0
+@todo:
+"""
+
+from source.tools.stats.loc_scale_estimator import  rob_mom
+import numpy as np
+
+
+
+def getconfi(distri, level , centre = None):
+    # inputs distribution , sigma level we want can be 1,2,3
+    # optinal input is centre. if given it will be used to calculate the limits otherwise the rob_mon will be used.
+    if level == 1:
+        s1  =   np.percentile(distri, [16,84], axis=0)
+
+    if level == 2:
+        s1  =   np.percentile(distri, [5,95], axis=0)
+
+    if level == 3:
+        s1  =   np.percentile(distri, [0.3,99.7], axis=0)
+
+    # If center is provided, take it as cen, otherwise use the median value of the data.
+    if centre is None:
+        loc = np.asarray(rob_mom(distri))[0]
+    else:
+        loc = centre
+
+    dis_right, dis_left =  s1[1]-loc,  loc-s1[0]
+
+    return dis_right, loc, dis_left
--- susana/getmr.py
+++ susana/getmr.py
@@ -10,8 +10,9 @@ from scipy import stats
 import sys
 sys.path.append('/Users/sbarros/Documents/work/python/photodynamic/lisa/')
 
-from source.tools.stats.loc_scale_estimator import  rob_mom
+#from source.tools.stats.loc_scale_estimator import  rob_mom
 
+from source.tools.distribution_anali  import getconfi
 
 #spectroscopic properties (value, uncertainty)
 Teff = (5770, 80)
@@ -31,12 +32,17 @@ model.fit()
 #model.plot_samples('radius')
 #model.plot_samples('mass')
 
+
 rad, sta = model.prop_samples('radius')
+'''
 s1  =   np.percentile(rad, [16, 50, 84], axis=0)
 mrad = np.asarray(rob_mom(rad))[0]
 rad_right, rad_left =  s1[2]-mrad,  mrad-s1[0]
 #map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                              #zip(*np.percentile(rad, [16, 50, 84], axis=0))
+the following line is equavalent to the lines comeneted above
+'''
+rad_right, mrad, rad_left = getconfi(rad, 1)
 
 print('rob mom radius condifence')
 print('%.2f^{+%.2f}_{-%.2f}' % (mrad, rad_right,rad_left))
@@ -48,10 +54,14 @@ plt.show()
 
 
 mass, sta = model.prop_samples('mass')
-
+'''
 s1  =   np.percentile(mass, [16, 50, 84], axis=0)
 mmas = np.asarray(rob_mom(mass))[0]
 mas_right, mas_left =  s1[2]-mmas,  mmas-s1[0]
+the following line is equavalent to the lines comeneted above
+'''
+mas_right,mmas ,  mas_left = getconfi(mass, 1)
+
 print('rob mom mass condifence')
 print('%.2f^{+%.2f}_{-%.2f}' % (mmas, mas_right, mas_left))
 print('mode')
@@ -64,10 +74,13 @@ plt.show()
 
 
 density = mass/rad**3.
-
+'''
 s1  =   np.percentile(density, [16, 50, 84], axis=0)
 mdens = np.asarray(rob_mom(density))[0]
 dens_right, dens_left =  s1[2]-mdens,  mdens-s1[0]
+the following line is equavalent to the lines comeneted above
+'''
+dens_right, mdens ,dens_left  = getconfi(density, 1)
 print('rob mom density condifence')
 print('%.2f^{+%.2f}_{-%.2f}' % (mdens, dens_right, dens_left))
 
