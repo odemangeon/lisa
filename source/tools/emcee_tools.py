9a8dc42591de4bc40a93052d917e2755d8767bc1
Olivier Demangeon <olivier.demangeon@lam.fr>
2017-03-31 11:17:18 +0100

    Update emcee_tools.py from master

--- source/tools/emcee_tools.py
+++ source/tools/emcee_tools.py
@@ -9,14 +9,39 @@ results.
 from logging import getLogger
 from matplotlib.pyplot import subplots  # , figure, plot, show
 from numpy import linspace
+from sys import stdout
 
 ## Logger Object
 logger = getLogger()
 
 
-def plot_chains(sampler, l_param_names, flat=False):
-    fig, ax = subplots(nrows=sampler.dim + 1, sharex=True, squeeze=True)
+# The incremental saving doesn't work because, it writes to the fiel but then I don't know how to
+# load it.
+# def explore(sampler, p0, nsteps, width=50, save_to_file=None):
+#     f = open(save_to_file, "w")
+#     f.close()
+#     for i, result in enumerate(sampler.sample(p0, iterations=nsteps, storechain=False)):
+#         position = result[0]
+#         f = open(save_to_file, "a")
+#         for k in range(position.shape[0]):
+#             f.write("{0:4d} {1:s}\n".format(k, " ".join([str(x) for x in position[k]])))
+#         f.close()
+#         n = int((width + 1) * float(i) / nsteps)
+#         stdout.write("\r[{0}{1}]".format('#' * n, ' ' * (width - n)))
+#     stdout.write("\n")
+
+def explore(sampler, p0, nsteps, width=50):
+    for i, result in enumerate(sampler.sample(p0, iterations=nsteps, storechain=True)):
+        n = int((width + 1) * float(i) / nsteps)
+        stdout.write("\r[{0}{1}]".format('#' * n, ' ' * (width - n)))
+    stdout.write("\n")
+
+
+def plot_chains(sampler, l_param_names, flat=False,
+                plot_height=2, plot_width=8, **kwargs_tl):
     nwalk = sampler.chain.shape[0]
+    fig, ax = subplots(nrows=sampler.dim + 1, sharex=True, squeeze=True,
+                       figsize=(plot_width, nwalk * plot_height))
     for k in range(nwalk):
         ax[0].set_title("lnpost")
         ax[0].plot(sampler.lnprobability[k, :], alpha=0.5)
@@ -25,16 +50,18 @@ def plot_chains(sampler, l_param_names, flat=False):
         for k in range(nwalk):
             ax[i + 1].plot(sampler.chain[k, :, i], alpha=0.5)
     ax[sampler.dim].set_xlabel("iteration")
+    fig.tight_layout(**kwargs_tl)
 
 
-def overplot_data_model(param, l_param_names, datasim_db, dataset_db, oversamp=10):
+def overplot_data_model(param, l_param_names, datasim_db, dataset_db, oversamp=10,
+                        plot_height=2, plot_width=8, **kwargs_tl):
     """param        np.array
        datasim_db   dataset_db in datasimulators
        dataset_db   dataset_db
     """
     l_datasets = dataset_db.get_datasets()
     ndataset = len(l_datasets)
-    fig, ax = subplots(nrows=ndataset)
+    fig, ax = subplots(nrows=ndataset, figsize=(plot_width, ndataset * plot_height))
     for i, dataset in enumerate(l_datasets):
         ax[i].set_title(l_datasets[i])
         kwargs = dataset.get_kwargs()
@@ -53,3 +80,4 @@ def overplot_data_model(param, l_param_names, datasim_db, dataset_db, oversamp=1
             idx_par.append(l_param_names.index(par))
         ax[i].plot(t, datasim_function(param[idx_par], t), "r-")
         ax[i].set_title(dataset.dataset_name)
+    fig.tight_layout(**kwargs_tl)
