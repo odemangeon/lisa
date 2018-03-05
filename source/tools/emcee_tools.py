7d9d27f1254037bfd331b7e17fe7ee75f715ae50
Olivier Demangeon <olivier.demangeon@lam.fr>
2018-03-05 14:16:33 +0000

    emcee_tools.py: uncomment the statsmodels import

--- source/tools/emcee_tools.py
+++ source/tools/emcee_tools.py
@@ -22,8 +22,7 @@ from dill import dump, load
 from os.path import isfile, join
 # import pprint
 
-# Temporary commenting.
-# from .stats.loc_scale_estimator import mad
+from .stats.loc_scale_estimator import mad
 from .time_series_toolbox import get_time_supersampled, average_supersampled_values
 from ..posterior.core.likelihood.jitter_noise_model import jitter_name
 from ..posterior.core.likelihood.manager_noise_model import Manager_NoiseModel
