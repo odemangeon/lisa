9f72c8fa5a405027f2c0c2439c51114941057dd8
Olivier Demangeon <olivier.demangeon@lam.fr>
2018-12-05 11:49:06 +0000

    Add print of iteration number in plot_chains command line util

--- script_and_functions/plot_chains.py
+++ script_and_functions/plot_chains.py
@@ -25,11 +25,14 @@ now = datetime.now()
 
 # Copy the .dat file
 datfile_root, datfile_ext = splitext(args.chain_datfile)
-copied_datefile =  datfile_root + "_tmp_{0.year}{0.month}{0.day}{0.hour}{0.minute}{0.second}".format(now) + datfile_ext
+copied_datefile = datfile_root + "_tmp_{0.year}{0.month}{0.day}{0.hour}{0.minute}{0.second}".format(now) + datfile_ext
 copyfile(args.chain_datfile, copied_datefile)
 
 # Read the chain dat file
 chains, lnpost, l_params = read_chaindatfile(copied_datefile)
+
+# Plot the number of iterations
+print("Current iteration number: {}".format(chains.shape[1]))
 
 # Produce trace plot
 plot_chains(chains, lnpost, l_params)
