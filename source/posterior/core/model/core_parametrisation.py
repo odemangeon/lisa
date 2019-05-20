5f3b2a7c97c3453e7c1ad6218d3ccd8e1bca8751
Olivier Demangeon <olivier.demangeon@lam.fr>
2019-05-20 16:49:15 +0100

    Bug fix in naming of dict key in save_parametrisation.

--- source/posterior/core/model/core_parametrisation.py
+++ source/posterior/core/model/core_parametrisation.py
@@ -99,6 +99,6 @@ class Core_Parametrisation(object):
 
     def save_parametrisation_kwargs(self, **kwargs):
         """Save the keyword arguments of the parmetrisation function in parametrisation_kwargs."""
-        self.parametrisation_kwargs["parameterisation"] = self.parametrisation
+        self.parametrisation_kwargs["parametrisation"] = self.parametrisation
         for key, value in kwargs.items():
             self.parametrisation_kwargs[key] = value
