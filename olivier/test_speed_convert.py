f419c7fee7c2b68691de8594e5e30990ef8748f5
Olivier Demangeon <olivier.demangeon@lam.fr>
2019-07-23 13:25:36 +0100

    Remove folders olivier and ocode that are not usefull anymore.

--- olivier/test_speed_convert.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Script to test the speed of the source.tools.convert function.
-
-Todo:
-    * Test speed and possible improvement of getaoverr
-"""
