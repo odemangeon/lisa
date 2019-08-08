831f8097f3f1c7ee45f88357d8b5b50d93e1b693
Olivier Demangeon <olivier.demangeon@lam.fr>
2019-08-08 14:47:38 +0100

    Give access to Posterior class in lisa/__init__.py

--- lisa/__init__.py
+++ lisa/__init__.py
@@ -3,18 +3,16 @@
 """
 lisa package.
 
-The objective of this package is to provides tools to interpret simultaneously light-curve and
- radial velocity data of the same star.
-
-@version 1.0 06/07/2016 ODE # First
-
-TODO:
-    - Everything
+The objective of this package is to provides a framework for posterior function generation. The main
+focus for now is the analysis of exoplanet datasets and in particular transit photometry and radial
+velocity data.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.5dev"
 
 # __all__ = []  # Put here the import classes or function from the modules or packages of lisa
 # from .data_interface import  # import the main classes or function defined in data_interface
+from .posterior.core.posterior import Posterior
 
-# NO CODE here expect what is above. If you what to had some code use a module of this package.
+__all__ = ["Posterior", ]
+# NO CODE here expect what is above.
