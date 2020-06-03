3068547a406b558036ccd899ea90754a67155dbe
Olivier Demangeon <olivier.demangeon@lam.fr>
2020-06-03 11:10:00 +0100

    Output residuals from overplot_data_model.

--- script_and_functions/script_chainanalysis.py
+++ script_and_functions/script_chainanalysis.py
@@ -436,12 +436,12 @@ if do_MComp:
         fitted_val_plot = np.array([df_fittedval.loc[param_name, "value"] for param_name in l_param_chainI])
     else:
         fitted_val_plot = fitted_values
-    et.overplot_data_model(param=fitted_values, l_param_name=l_param_chainI,
-                           datasim_dbf=post_instance.datasimulators,
-                           dataset_db=post_instance.dataset_db,
-                           post_instance=post_instance,
-                           datasim_kwargs=kwargs_datasim,
-                           oversamp=oversamp_MComp)
+    residuals = et.overplot_data_model(param=fitted_values, l_param_name=l_param_chainI,
+                                       datasim_dbf=post_instance.datasimulators,
+                                       dataset_db=post_instance.dataset_db,
+                                       post_instance=post_instance,
+                                       datasim_kwargs=kwargs_datasim,
+                                       oversamp=oversamp_MComp, return_resi=True)
     if save_plots:
         pl.savefig(join(output_folders["plots"], f"data_comparison{extension_outputs}.pdf"))
     else:
