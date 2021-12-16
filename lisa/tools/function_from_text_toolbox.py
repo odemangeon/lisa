319772460530ae6ea2648f54a8482490257987f5
odemangeon <olivier.demangeon@astro.up.pt>
2021-12-16 11:36:01 +0000

    Create FunctionBuilder

--- lisa/tools/function_from_text_toolbox.py
+++ lisa/tools/function_from_text_toolbox.py
@@ -12,6 +12,10 @@ from text.
     -
 """
 from collections import Iterable, OrderedDict
+from copy import copy
+from logging import getLogger
+
+logger = getLogger()
 
 
 ## Name of the model parameter vector
@@ -23,6 +27,359 @@ key_mand_kwargs = "mandatory_kwargs"
 key_opt_kwargs = "optional_kwargs"
 
 argskwargs = ", *args, **kwargs"
+
+
+class FunctionBuilder(object):
+    """docstring for FunctionBuilder."""
+
+    def __init__(self, parameter_vector_name=par_vec_name):
+        """Classes designed to keep track of the arguments of the functions that will be produced.
+
+        Instances from this class can be used to deal with several functions at the same time.
+        It stores the definition of the arguments of the future function and its name
+
+        The arguments of the function are divided in three category:
+        - The parameter vector: one single argument wich is the first argument of the function and is
+        a numpy array containing all the jumping parameters of the model. The name of this parameter
+        vector is provided via parameter_vector_name;
+        - The mandatory arguments: as many arguments as need that are not jumping parameter of the model
+        These arguments are mandatory in the sense that they don't have a default value;
+        - The optional arguments: as many arguments as need that are not jumping parameter of the model
+        These arguments are optional in the sense that they have a default value if they are not provided;
+
+        All these informations are stored in the _database attribute of the instance, but this should
+        in principle not be accessed directly, but queried and filled with the following methods:
+        - TBD
+
+        Arguments
+        ---------
+        parameter_vector_name   : str
+            Str to be used for the parameter vector of the model
+        """
+        super(FunctionBuilder, self).__init__()
+        self.__parameter_vector_name = parameter_vector_name
+        self._database = OrderedDict()  # The keys are a short name for the function to be created
+
+    @property
+    def parameter_vector_name(self):
+        """str of the model parameter vector in the argument of the functions
+        """
+        return self.__parameter_vector_name
+
+    @property
+    def l_function_shortname(self):
+        """List of the short names of the functions being tracked
+        """
+        return list(self._database.keys())
+
+    def add_new_function(self, shortname, parameters=None, mandatory_args=None, optional_args=None, full_function_name=None,
+                         ldict=None):
+        """Add new function to track
+
+        Arguments
+        ---------
+        shortname           : str / None
+            Short name of the function
+        parameters          : List of Parameter / None
+            List of the parameters in the model parameter vector
+        mandatory_args      : List of str / None
+            List of the mandatory argument names
+        optional_args       : dictionary
+            Pair of optional argument name and their default value
+        full_function_name  : str
+            Full name of the function
+        ldict               : dictionary
+            Local dictionary of the function
+        """
+        self._database[shortname] = {"parameters": [],  # List of the model (jumping) parameter instances for which the value should be provided to the in the parameter vector that is provided to the model
+                                     "mandatory_args": [],  # List of the str with the name of the other mandatory arguments (besides the parameter vector) of by the function.
+                                     "optional_args": OrderedDict(),  # List of tuples with two arguments: a str giving the name of the other optional arguments of by the function and their default value
+                                     "full_name": None,  # function full name, if None full name equal short name
+                                     "ldict": [],  # Local dictionary for the function
+                                     "body_text": ""  # Text of the body of the function
+                                     }
+        if parameters is not None:
+            for parameter in parameters:
+                self.add_parameter(parameter=parameter, function_shortname=shortname)
+        if mandatory_args is not None:
+            for argument_name in mandatory_args:
+                self.add_mandatory_argument(argument_name=argument_name, function_shortname=shortname)
+        if optional_args is not None:
+            for argument_name, default_value in optional_args:
+                self.add_optional_argument(argument_name=argument_name, default_value=default_value,
+                                           function_shortname=shortname)
+        if full_function_name is not None:
+            self.set_function_fullname(full_name=full_function_name, shortname=shortname)
+        if ldict is not None:
+            for key, value in ldict.items():
+                self.add_variable_to_ldict(variable_name=key, variable_content=value, shortname=shortname)
+
+    def copy_function(self, shortname_src, shortname_copy, full_function_name_copy=None):
+        """Copy a function
+
+        Arguments
+        ---------
+        shortname_src           : str
+            Short name of the source function that you want to copy
+        shortname_copy          : str
+            Short name of the new function that you want to the source function to by copied into
+        full_function_name_copy : str
+            Full name of the new function that you want to the source function to by copied into
+        """
+        self.add_new_function(shortname=shortname_copy, parameters=self.get_parameter_vector(function_shortname=shortname_src),
+                              mandatory_args=self.get_l_mandatory_argument(function_shortname=shortname_src),
+                              optional_args={arg: self.get_default_value_4_arg(argument_name=arg, function_shortname=shortname_src) for arg in self.get_l_optional_argument_name(function_shortname=shortname_src)},
+                              full_function_name=full_function_name_copy,
+                              ldict=self.get_ldict(shortname=shortname_src))
+
+    def add_parameter(self, parameter, function_shortname, exist_ok=False):
+        """Add a parameter to the parameter vector of a function
+
+        Arguments
+        ---------
+        parameter           : Parameter
+            Parameter of the model
+        function_shortname  : str
+            Short name of the function.
+        exist_ok            : bool
+            If True the function will not produce a warning if the parameter already exists in the function
+        """
+        if parameter not in self._database[function_shortname]["parameters"]:
+            self._database[function_shortname]["parameters"].append(parameter)
+        else:
+            if not(exist_ok):
+                logger.warning(f"Parameter {parameter} already exists for function {function_shortname}")
+
+    def add_mandatory_argument(self, argument_name, function_shortname, exist_ok=False):
+        """Add a mandatory argument to a function
+
+        Arguments
+        ---------
+        argument_name       : str
+            Name of the argument to add
+        function_shortname  : str
+            Short name of the function
+        exist_ok            : bool
+            If True the function will not produce a warning if the argument already exists in the function
+        """
+        if argument_name not in self._database[function_shortname]["mandatory_args"]:
+            self._database[function_shortname]["mandatory_args"].append(argument_name)
+        else:
+            if not(exist_ok):
+                logger.warning(f"Mandatory argument {argument_name} already exists for function {function_shortname}")
+
+    def add_optional_argument(self, argument_name, default_value, function_shortname):
+        """Add an optional argument to a function
+
+        Arguments
+        ---------
+        argument_name       : str
+            Name of the argument to add
+        default_value       : "value"
+            Default value of the argument
+        function_shortname  : str
+            Short name of the function
+        """
+        if argument_name not in self._database[function_shortname]["optional_args"]:
+            self._database[function_shortname]["optional_args"][argument_name] = default_value
+        else:
+            if default_value == self.get_default_value_4_argument(argument_name=argument_name,
+                                                                  function_shortname=function_shortname):
+                logger.warning(f"Optional argument {argument_name} already exists for function {function_shortname}")
+            else:
+                logger.error(f"Optional argument {argument_name} already exists for function {function_shortname} with a different default value")
+
+    def get_parameter_vector(self, function_shortname):
+        """Return the list of parameters in the parameter vector of a function
+
+        Arguments
+        ---------
+        function_shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        l_parameter : List of Parameter
+            List of Model parameter
+        """
+        return copy(self._database[function_shortname]["parameters"])
+
+    def get_l_mandatory_argument(self, function_shortname):
+        """Return the list of mandatory arguments of a function
+
+        Arguments
+        ---------
+        function_shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        l_argument_name : list of str
+            List of the name of the mandatory arguments
+        """
+        return copy(self._database[function_shortname]["mandatory_args"])
+
+    def get_l_optional_argument_name(self, function_shortname):
+        """Return the list of the name of the optional arguments of a function
+
+        Arguments
+        ---------
+        function_shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        l_argument_name : list of str
+            List of the name of the optional arguments
+        """
+        return list(self._database[function_shortname]["optional_args"].keys())
+
+    def get_default_value_4_argument(self, argument_name, function_shortname):
+        """Return the default value for an given optional argument
+
+        Arguments
+        ---------
+        argument_name       : str
+            Name of the argument
+        function_shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        default_value   : ?
+            Default value of the argument
+        """
+        return self._database[function_shortname]["optional_args"][argument_name]
+
+    def get_function_fullname(self, shortname):
+        """Return the full name of a function
+
+        Arguments
+        ---------
+        shortname   : str
+            Short name of the function
+
+        Return
+        ------
+        full_name   : str
+            Full name of the function
+        """
+        if self._database[shortname]["full_name"] is not None:
+            return self._database[shortname]["full_name"]
+        else:
+            return shortname
+
+    def set_function_fullname(self, full_name, shortname):
+        """Set the full name of a function
+
+        Arguments
+        ---------
+        full_name   : str
+            Full name of the function
+        shortname   : str
+            Short name of the function
+        """
+        self._database[shortname]["full_name"] = full_name
+
+    def add_variable_to_ldict(self, variable_name, variable_content, function_shortname):
+        """Add a variable to the local dictionary of a function
+
+        Arguments
+        ---------
+        variable_name       : str
+            Variable name
+        variable_content    : ?
+            Content of the variable
+        function_shortname  : str
+            Short name of the function
+        """
+        if variable_name not in self._database[function_shortname]["ldict"]:
+            self._database[function_shortname]["ldict"][variable_name] = variable_content
+        else:
+            if self._database[function_shortname]["ldict"][variable_name] == variable_content:
+                logger.warning(f"Variable {variable_name} already exists in ldict of function {function_shortname}")
+            else:
+                logger.warning(f"Variable {variable_name} already exists in ldict of function {function_shortname} with a different content")
+
+    def get_ldict(self, shortname):
+        """Get the local dictionary of a function
+
+        Arguments
+        ---------
+        shortname   : str
+            Short name of the function
+
+        Return
+        ------
+        ldict   : dictionary
+            local dictionary of the function
+        """
+        return copy(self._database[shortname]["ldict"])
+
+    def get_function_header(self, shortname):
+        """Return the string of the function header
+
+        Arguments
+        ---------
+        shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        function header : str
+            Str giving the header (1st line) of the function definition
+        """
+        mand_args = ','.join(self.get_l_mandatory_argument(function_shortname=shortname))
+        if mand_args != "":
+            mand_args = ", " + mand_args
+        opt_args = ','.join([f"{arg}={self.get_default_value_4_arg(argument_name=arg, function_shortname=shortname)}" for arg in self.get_l_optional_argument_name(function_shortname=shortname)])
+        if opt_args != "":
+            opt_args = ", " + opt_args
+        return f"def {self.get_function_fullname(shortname=shortname)}({self.parameter_vector_name}{mand_args}{opt_args}):"
+
+    def get_text_4_parameter(self, parameter, function_shortname):
+        """Return the text for the model parameter of a function
+
+        Arguments
+        ---------
+        parameter           : Parameter
+            Model parameter
+        function_shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        parameter_in_param_vect : str
+            Str providing the parameter in the model parameter vector
+        """
+        return f"{self.parameter_vector_name}[{self.get_parameter_vector(function_shortname=function_shortname).index(parameter)}]"
+
+    def get_body_text(self, function_shortname):
+        """Return the text of the body of the function
+
+        Arguments
+        ---------
+        function_shortname  : str
+            Short name of the function
+
+        Return
+        ------
+        body_text   : str
+            Text of the body of the function
+        """
+        return copy(self._database[function_shortname]["body_text"])
+
+    def add_to_body_text(self, text, function_shortname):
+        """Return the text of the body of the function
+
+        Arguments
+        ---------
+        text                : str
+            Text to be added to the body of the function
+        function_shortname  : str
+            Short name of the function
+        """
+        self._database[function_shortname]["body_text"] += text
 
 
 def init_arglist_paramnb_arguments_ldict(key_param, keys, key_mand_kwargs=None, key_opt_kwargs=None,
