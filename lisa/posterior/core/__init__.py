31036f8fa194c04f7887620daa8c2b081ad2d0e9
odemangeon <olivier.demangeon@astro.up.pt>
2021-12-16 15:06:58 +0000

    Define function_whole_shortname

--- lisa/posterior/core/__init__.py
+++ lisa/posterior/core/__init__.py
@@ -6,3 +6,6 @@ core package for model definition.
 The objective of this package is to provides the core classes and functions to create a
 system model.
 """
+
+# Short name of the functions (datasimulators, priors, likelihoods, ...) that correspond to the whole model
+function_whole_shortname = "whole"
