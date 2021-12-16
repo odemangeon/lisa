2c0783248e592f9d9cd630595efd38c49a0a3d57
odemangeon <olivier.demangeon@astro.up.pt>
2021-12-16 15:06:34 +0000

    Define par_vec_name

--- lisa/posterior/core/model/__init__.py
+++ lisa/posterior/core/model/__init__.py
@@ -6,3 +6,5 @@ core package for model definition.
 The objective of this package is to provides the core classes and functions to create a
 system model.
 """
+# Name of parameter vector of the model
+par_vec_name = "p_vect"
