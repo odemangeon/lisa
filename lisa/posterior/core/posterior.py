c45960cf7d29aa493cb25bbce7776bc9f9405fd6
odemangeon <olivier.demangeon@astro.up.pt>
2022-02-23 23:23:57 +0000

    Removed unused line

--- lisa/posterior/core/posterior.py
+++ lisa/posterior/core/posterior.py
@@ -588,8 +588,8 @@ class Posterior(DatasetDbAttr, Named, RunFolder, Instmodel4DatasetAttr, DstDbLoc
         self.load_datasetsfile(dico["dataset_file"])
         # define model
         # Temporary fix for define_model not taking transit_model and rv_model as argument anymore.
-        transit_model = dico["model_kwargs"].pop("transit_model", None)  # For compatibility with previsous version
-        rv_model = dico["model_kwargs"].pop("rv_model", None)  # For compatibility with previsous version
+        # transit_model = dico["model_kwargs"].pop("transit_model", None)  # For compatibility with previsous version
+        # rv_model = dico["model_kwargs"].pop("rv_model", None)  # For compatibility with previsous version
         self.define_model(category=dico["model_category"], name=dico["object_name"],
                           **dico["model_kwargs"])
         # Automatic model_init: Set parameterisation, set param_file and inst_cat_param_file and load them
