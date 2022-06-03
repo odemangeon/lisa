50c6e8e40ed945291a5c9abd9abcd91f20ffd436
odemangeon <olivier.demangeon@astro.up.pt>
2022-06-03 15:58:14 +0100

    Modify LikelihoodDocFunc to reflect the modification made to DocFunction and DatasimDocFunc

--- lisa/posterior/core/likelihood/likelihood_docfunc.py
+++ lisa/posterior/core/likelihood/likelihood_docfunc.py
@@ -5,55 +5,66 @@ likelihood documented function module.
 """
 
 from ..model.datasim_docfunc import DatasimDocFunc
-from ....tools.function_w_doc import DocFunction
 
 ## String used for the noise model column in ouput_info of LikelihoodDocFunc
-noisemod_key = "noise model"
+noisemod_key = "noise_model"
 
 
-class LikelihoodDocFunc(DatasimDocFunc, DocFunction):
+class LikelihoodDocFunc(DatasimDocFunc):
     """docstring for LikelihoodDocFunc."""
-    def __init__(self, function, params_model, include_dataset_kwarg, mand_kwargs=None,
-                 opt_kwargs=None, output_info=None):
+
+    def __init__(self, function, param_model_names_list, params_model_vect_name, inst_cats_list, inst_model_fullnames_list,
+                 dataset_names_list, noisemodel_names_list,
+                 include_dataset_kwarg, mand_kwargs_list=None, opt_kwargs_dict=None):
         """Initialise the likelihood doc function.
 
-        :param function function: function of the likelihood
-        :param Iterable_of_string params_model: Iterable giving the ordered list of the model
-            parameters as they should be provided to the function.
-        :param bool include_dataset_kwarg: Boolean saying if the function include the dataset
-            keyword arguments.
-        :param string/None mand_kwargs: String giving the way the mandatory dataset_kwargs
-            (like the time vector or a time reference) should be given to function.
-            If None it means that the dataset kwargs are already included in function and should not
-            be provided.
-        :param string/None opt_kwargs: String giving the way the optional dataset_kwargs
-            (like the time vector or a time reference) should be given to function.
-            If None it means that the dataset kwargs are already included in function and should not
-            be provided.
-        :param pandas.DataFrame output_info: Table which provide the instrument category, model,
-            and noise models used for the output
+        Arguments
+        ---------
+        function                    : function object
+            function of the likelihood
+        param_model_names_list      : list of str
+            List of the name of the model parameters as they should be provided to the function in the
+            first argument
+        params_model_vect_name      : str
+            Name of the argument used for the model parameter vector
+        inst_cats_list              : Iterable
+            Iterable giving the ordered list of the outputs instrument categories.
+            It has to match both inst_model_fullnames_list and dataset_names_list
+        inst_model_fullnames_list   : Iterable_of_string/string
+            If string, the likelihood simulate only one instrument model and this is its full
+            name.
+            If Iterable, give the list of full names of the instrument models simulated.
+            This list will be matched to dataset_names_list and inst_cats_list and noisemodel_names_list.
+        dataset_names_list          : Iterable_of_string/string
+            If string, the likelihood simulate only one dataset and this is its name
+            If Iterable, give the list of names of the datasets simualted. This list will be matched
+            to inst_model_fullnames_list and inst_cats_list and noisemodel_names_list.
+        noisemodel_names_list       : Iterable_of_string/string
+            If string, the likelihood simulate only one dataset/instrument model and this is the name
+            of the noise model associated.
+            If Iterable, give the list of names of the noise models. This list will be matched
+            to inst_model_fullnames_list and inst_cats_list and dataset_names_list.
+        include_dataset_kwarg       : bool
+            Boolean saying if the function include the dataset(s) keyword arguments.
+        mand_kwargs_list            : List of str
+            List of the names of the mandatory arguments besides the name of the model parameter vector
+            (like the time vector or a time reference) which should be given to function after the
+             model parameter vector.
+        opt_kwargs_dict             : dict
+            Dictionary whose keys are the name of the optional arguments of the function and the values
+            are the default values for these arguments
         """
-        # Initialise the params_model property
-        self._init_params_model(params_model)
-
-        # Get the arg_list input for DocFunction and initialise the include_dataset_kwarg property
-        arg_list = self._init_arg_list(params_model, mand_kwargs, opt_kwargs)
-
-        # Intialise output_info property
-        self.__output_info = output_info
-
-        # For now Likelihood function cannot be multi_ouput
-        self.__multi_output = False
-
-        DocFunction.__init__(self, function=function, arg_list=arg_list)
-
-    def _init_params(self, params):
-        self.__params = params
+        super(LikelihoodDocFunc, self).__init__(function=function, param_model_names_list=param_model_names_list,
+                                                params_model_vect_name=params_model_vect_name, inst_cats_list=inst_cats_list,
+                                                inst_model_fullnames_list=inst_model_fullnames_list,
+                                                dataset_names_list=dataset_names_list, include_dataset_kwarg=include_dataset_kwarg,
+                                                mand_kwargs_list=mand_kwargs_list, opt_kwargs_dict=opt_kwargs_dict)
+        self.__output_info[noisemod_key] = noisemodel_names_list
 
     @property
-    def output_info(self):
-        """Return a table with the instrument category, model and datasets used."""
-        return self.__output_info
+    def noisemodel_names_list(self):
+        """Return the Serie of noise model names  used for the ouputs."""
+        return self.output_info[noisemod_key]
 
     @property
     def noutput(self):
@@ -62,8 +73,3 @@ class LikelihoodDocFunc(DatasimDocFunc, DocFunction):
         For now Likelihood function cannot be multi_ouput
         """
         return 1
-
-    @property
-    def multi_output(self):
-        """If True the datasim function simulates several outputs."""
-        return self.__multi_output
