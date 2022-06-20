883ae6162e88a0168ee22d3315dc2e0e21ec46dd
odemangeon <olivier.demangeon@astro.up.pt>
2022-06-20 15:34:49 +0100

    New DecorrelationLikelihood Class and SplineDecorrelation Class

---
+++ lisa/posterior/core/likelihood/core_decorrelation_likelihood.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# -*- coding:  utf-8 -*-
+"""
+Decorrelation model module.
+
+TODO:
+- If I want several decorrelation methods beside the Linear Decorrelation and do not want to have to
+modify this module than I will need to implement a decorrelation method manager.
+"""
+from logging import getLogger
+
+from ....tools.metaclasses import MandatoryReadOnlyAttrAndMethod
+from ....tools.miscellaneous import spacestring_like
+from ..dataset_and_instrument.manager_dataset_instrument import Manager_Inst_Dataset
+
+
+## Logger object
+logger = getLogger()
+
+mgr_inst_dst = Manager_Inst_Dataset()
+mgr_inst_dst.load_setup()
+
+
+class Core_DecorrelationLikelihood(object, metaclass=MandatoryReadOnlyAttrAndMethod):
+    """docstring for Core_DecorrelationLikelihood class, Parent class of all Decorrelation likelihood Class"""
+
+    ## List of mandatory arguments which have to be defined in the subclasses.
+    # For example "category" is in this list. It has to be defined in the subclass as a class
+    # attribute like this:
+    # __category__ = "ModelCategory"
+    # It then be read as self.category
+    __mandatoryattrs__ = ["category", "format_config_dict", "l_required_inddatasetkwarg_keys",
+                          "l_required_datasetkwarg_keys"]
+    # category: String which designate the decorrelation model (for example: "linear"). To choose the
+    #   decorrelation model to be used, the user will use this string.
+    # format_config_dict is a strong to be used as the example of how to specify the dictionary in the
+    #   Instrument specific parameter file
+    __mandatorymeths__ = ["apply_parametrisation", "create_decorrelation_likelihood"]
+    # apply_parametrisation: Method that creates the parameters necessary for the decorrelation model
+    #  for each instrument model object of the instrument category to which this decorrelation model applies
+    #  The arguments must be inst_mod_obj, the Instrument model object and decorrelation_config_inst_decorr
+    #  the dictionary that contains the configuration of the decorrelation model for the instrument model object
+    #  considered
+    # get_text_decorrelation: This function produces the text for the decorrelation model for all decorrelation
+    #  variable using a given decorrelation category
+
+    @classmethod
+    def create_text_decorr_paramfile(cls, inst_mod_obj, decorrelation_config_inst, tab=""):
+        """Create the text for the linear decorrelation of the data of a given instrument model object.
+
+        Method which create the text to be written in an instrument category specific paramfile to contain
+        the parameterisation of the decorrelation models for each instrument model of the category.
+
+        This function is used by Core_InstCat_Model.create_text_paramfile_decorrelation
+
+        Arguments
+        ---------
+        inst_mod_obj                : Instrument Model
+            Instrument model object used for the data that you are trying to decorrelate
+        decorrelation_config_inst   : dict
+            dictionary with the current decorrelation config parameter for the provided of the instrument
+            model object provided and the linear decorrelation
+        tab                         : str
+            tabulations/space characters that needs to be added at the beginning of each line.
+
+        Returns
+        -------
+        text_decorr : string
+            Text to add to the parameter
+        """
+        text_decorr_vars = ""
+        tab_newline = f"{tab}" + spacestring_like(f"'{cls.category}':  ")
+        for inst_mod_obj_decorr_var, dico_config in decorrelation_config_inst.items():
+            if len(text_decorr_vars) > 0:
+                text_decorr_vars += tab_newline
+            text_decorr_vars += f"'{inst_mod_obj_decorr_var}': {dico_config},\n"
+        return f"'{cls.category}': " + "{" + f"{text_decorr_vars}\n{tab_newline}" + "}"
+
+    @classmethod
+    def load_text_decorr_paramfile(cls, inst_mod_obj, decorrelation_config_inst_decorr_paramfile, decorrelation_config_inst_decorr,
+                                   skip_load=False):
+        """load the parametrisation for the decorrelation of the instrument model from the inst cat param file.
+
+        Method which load the dictionary written in an instrument category
+        specific paramfile and which contains the parameterisation of the decorrelation models for each
+        for each instrument model of the category.
+
+        This function is used by Core_InstCat_Model.load_config_decorrelation
+        It is advised to overload this function in the child Core_DecorrelationModel class to at make
+        some additional checks on the content of decorrelation_config_inst_decorr_paramfile.
+
+        Arguments
+        ---------
+        inst_mod_obj                                : Core_InstrumentModel
+            Instrument model object of which you want to load the decorrelation parameterisation
+        decorrelation_config_inst_decorr_paramfile  : dict
+            Dictionary providing the configuration of the decorrelation for the instrument model inst_mod_obj
+            and the current decorrelation method.
+            The expected format is
+        decorrelation_config_inst_decorr            : dict
+            Dictionary where the decorrelation configuration is stored for the instrument model inst_mod_obj
+            and the current decorrelation method.
+        skip_load                                   : bool
+            If True the function will not load into decorrelation_config_inst_decorr. This option is to
+            be used when you are overloading this function to performed the loading in the overloading function
+            while keeping the checl
+        """
+        # Check that each instrument model object of the decorrelation variable exists
+        for inst_mod_obj_decorr_var_name in decorrelation_config_inst_decorr_paramfile.keys():
+            inst_mod_obj_decorr_var_name_info = mgr_inst_dst.interpret_instmod_fullname(instmod_fullname=inst_mod_obj_decorr_var_name, raise_error=True)
+        if not(skip_load):
+            decorrelation_config_inst_decorr = decorrelation_config_inst_decorr_paramfile
---
+++ lisa/posterior/exoplanet/likelihood/decorrelation/spline_decorrelation.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+# -*- coding:  utf-8 -*-
+"""
+Decorrelation model module.
+"""
+from logging import getLogger
+from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import LSQUnivariateSpline  # This should allow to specify the knots
+from numpy import concatenate, argsort
+
+from ....core.likelihood.core_decorrelation_likelihood import Core_DecorrelationLikelihood
+
+
+## Logger object
+logger = getLogger()
+
+tab = "    "
+
+
+class SplineDecorrelation(Core_DecorrelationLikelihood):
+    """docstring for SplineDecorrelation."""
+
+    # Mandatory attributes from Core_DecorrelationModel
+    __category__ = "spline"
+    __format_config_dict__ = "{'quantity': 'raw', 'spline_type': 'UnivariateSpline', 'spline_kwargs': {}}"
+    __allowed_quantity_strs__ = ['raw', ]
+    __modelpart_4_decorrlikelihood__ = "add"
+
+    ##  List of keys (string) giving the dataset kwargs of the indicators dataset required for
+    # the computation of the decorrelation model.
+    # This info will be used to get these dataset_kwargs out of the indicator datasets and produce the
+    # inddataset_kwargs variable which will contain all the dataset_kwargs of the indicators required
+    # for the decorrelation prior to the likelihood computation.
+    __l_required_inddatasetkwarg_keys__ = ["data", ]
+
+    # List of keys (string) giving the dataset kwargs of the datasets (not the indicator
+    # datasets, the datasets associated to the indicator dataset for the decorrelation) required for
+    # the computation of the decorrelation model.
+    # This info will be used to get these dataset_kwargs out of the dataset and produce the dataset_kwargs
+    # variable which will contain all the dataset_kwargs required for the decorrelation prior to the
+    # likelihood computation.
+    __l_required_datasetkwarg_keys__ = ["data", ]
+
+    # Mandatory method from Core_DecorrelationModel
+    @classmethod
+    def load_text_decorr_paramfile(cls, inst_mod_obj, decorrelation_config_inst_decorr_paramfile, decorrelation_config_inst_decorr):
+        """load the parametrisation for the decorrelation of one instrument model from the inst cat param file.
+
+        This function is used by Core_InstCat_Model.load_config_decorrelation
+        This function checks that the inputs provided are valid and store them in decorrelation_config_inst_decorr
+
+        Arguments
+        ---------
+        inst_mod_obj                                : Core_InstrumentModel
+            Instrument model object of which you want to load the decorrelation parameterisation
+        decorrelation_config_inst_decorr_paramfile  : dict
+            Dictionary providing the configuration of the decorrelation for the instrument model inst_mod_obj
+            and the current decorrelation method.
+        decorrelation_config_inst_decorr            : dict
+            Dictionary where the decorrelation configuration is stored for the instrument model inst_mod_obj
+            and the current decorrelation method.
+            Structure:
+               key0: do
+               value0: bool, say if the decorelation should be performed
+               Keyn: decorrelation model name
+               valuen: dict, parameters of the decorrelation model
+        """
+        super(SplineDecorrelation, cls).load_text_decorr_paramfile(inst_mod_obj=inst_mod_obj,
+                                                                   decorrelation_config_inst_decorr_paramfile=decorrelation_config_inst_decorr_paramfile,
+                                                                   decorrelation_config_inst_decorr=decorrelation_config_inst_decorr,
+                                                                   skip_load=True)
+        for inst_mod_obj_decorr_var_name in decorrelation_config_inst_decorr_paramfile.keys():
+            # Check that the "quantity" and "spline_kwargs" are in the dictionary
+            if not(all([key in decorrelation_config_inst_decorr_paramfile[inst_mod_obj_decorr_var_name] for key in ["quantity", "spline_kwargs"]])):
+                raise ValueError(f"The dictionary for the configuration of the spline decorrelation of {inst_mod_obj.full_name}"
+                                 f" with {inst_mod_obj_decorr_var_name} must include the keys 'quantity' and 'spline_kwargs'.")
+            quantity = decorrelation_config_inst_decorr_paramfile[inst_mod_obj_decorr_var_name]["quantity"]
+            spline_type = decorrelation_config_inst_decorr_paramfile[inst_mod_obj_decorr_var_name].get("spline_type", "UnivariateSpline")
+            spline_kwargs = decorrelation_config_inst_decorr_paramfile[inst_mod_obj_decorr_var_name]["spline_kwargs"]
+            # Check that the "quantity" value is valid
+            if quantity not in cls.__allowed_quantity_strs__:
+                raise ValueError(f"'quantity' for the configuration of the spline decorrelation of {inst_mod_obj.full_name}"
+                                 f" with {inst_mod_obj_decorr_var_name} must be in {cls.__allowed_quantity_strs__}.")
+            # If quantity is raw, check that "match datasets" is in the dictionary
+            if quantity == "raw":
+                if "match datasets" not in decorrelation_config_inst_decorr_paramfile[inst_mod_obj_decorr_var_name]:
+                    raise ValueError(f"Since quantity is {quantity}, the dictionary for the configuration of the spline decorrelation of {inst_mod_obj.full_name}"
+                                     f" with {inst_mod_obj_decorr_var_name} must include the key 'match datasets'.")
+            # Check spline_type
+            if spline_type not in ["UnivariateSpline", "LSQUnivariateSpline"]:
+                raise ValueError(f"Spline_type for the decorrelation of {inst_mod_obj.full_name}"
+                                 f" with {inst_mod_obj_decorr_var_name} is invalid. Must be in ['UnivariateSpline', 'LSQUnivariateSpline'] got {spline_type}.")
+            # TODO: Check content of spline_kwargs which will be passed to scipy.interpolate.UnivariateSpline
+            # Store the decorrelation configuration
+            decorrelation_config_inst_decorr[inst_mod_obj_decorr_var_name] = decorrelation_config_inst_decorr_paramfile[inst_mod_obj_decorr_var_name]
+
+    @classmethod
+    def apply_parametrisation(cls, inst_mod_obj, decorrelation_config_inst_decorr, model_part=""):
+        """Apply the parametrisation for the decorrelation to an instrument model.
+
+        This function is used by parametrisation_gravgroup.apply_instmodel_parametrisation.
+        For now there is no parameters for this type of decorrelation
+
+        Arguments
+        ---------
+        inst_mod_obj                        : Instrument_Model instance
+            Instrument model object to which you want to apply the parametrisation associated to the
+            decorrelation model
+        decorrelation_config_inst_decorr    : dict
+            Dictionary where the decorrelation configuration is stored for the instrument model inst_mod_obj
+            and the current decorrelation method.
+            Structure:
+               key0: do
+               value0: bool, say if the decorelation should be performed
+               Keyn: decorrelation model name
+               valuen: dict, parameters of the decorrelation model
+        model_part                          : str
+            String giving the model part concerned
+        """
+        pass
+
+    def create_decorrelation_likelihood(function_builder, function_shortname, inst_model_obj, ind_instmodel_obj,
+                                        dico_decorr_ind, dico_decorr_config_ind, l_dataset_name_4_instmod,
+                                        l_dataset_name, l_paramsfullname_likelihood, dataset_kwargs, inddataset_kwargs):
+        """
+        """
+        # You need to store in ldict spline_kargs_{ind_inst_model_fullname.strip('-')}
+        function_builder.add_variable_to_ldict(variable_name="concatenate", variable_content=concatenate, function_shortname=function_shortname, exist_ok=True, overwrite=False)
+        function_builder.add_variable_to_ldict(variable_name="l_dataset_name", variable_content=l_dataset_name, function_shortname=function_shortname, exist_ok=True, overwrite=False)
+        function_builder.add_variable_to_ldict(variable_name=f"spline_kargs_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}", variable_content=dico_decorr_config_ind['spline_kwargs'], function_shortname=function_shortname, exist_ok=False, overwrite=False)
+        function_builder.add_variable_to_ldict(variable_name=f"l_inddataset_name_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}", variable_content=dico_decorr_ind["l_inddataset_name"], function_shortname=function_shortname, exist_ok=False, overwrite=False)
+        spline_type = dico_decorr_config_ind.get("spline_type", "UnivariateSpline")
+        if spline_type == "UnivariateSpline":
+            function_builder.add_variable_to_ldict(variable_name="UnivariateSpline", variable_content=UnivariateSpline, function_shortname=function_shortname, exist_ok=True, overwrite=False)
+        elif spline_type == "LSQUnivariateSpline":
+            function_builder.add_variable_to_ldict(variable_name="LSQUnivariateSpline", variable_content=LSQUnivariateSpline, function_shortname=function_shortname, exist_ok=True, overwrite=False)
+        x_vect = concatenate([inddataset_kwargs[inddataset_name]['data'] for inddataset_name in dico_decorr_ind["l_inddataset_name"]])
+        idx_sort_x_vect = argsort(x_vect)
+        function_builder.add_variable_to_ldict(variable_name=f"idx_sort_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}", variable_content=idx_sort_x_vect, function_shortname=function_shortname, exist_ok=False, overwrite=False)
+        function_builder.add_to_body_text(f"{tab}sp_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name} = {spline_type}(x=concatenate([inddataset_kwargs[inddataset_name]['data'] for inddataset_name in l_inddataset_name_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}])[idx_sort_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}], y=concatenate([dataset_kwargs[l_dataset_name[idx]]['data'] - sim_data[idx] for idx in {dico_decorr_ind['l_idx_simdata']}])[idx_sort_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}], **spline_kargs_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name})\n", function_shortname=function_shortname)
+        decorrtext_4_dataset = {dst_name: "" for dst_name in l_dataset_name_4_instmod}
+        for dataset_name in l_dataset_name_4_instmod:
+            decorrtext_4_dataset[dataset_name] = f"sp_{inst_model_obj.full_code_name}_{ind_instmodel_obj.full_code_name}(inddataset_kwargs['{dico_decorr_config_ind['match datasets'][dataset_name]}']['data'])"
+        return decorrtext_4_dataset, l_paramsfullname_likelihood
