276c8f9e0c3153020bbfc346795619af83a28dd8
odemangeon <olivier.demangeon@astro.up.pt>
2022-09-21 15:00:52 +0100

    added wanted param_files

---
+++ tests/dev_tests/gravgroup_param_file_wanted.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:  utf-8 -*-
+
+# Orbital models
+orbital_model = {'b': {'model4instrument': {'LC_CHEOPS_instTr0': '',
+                                            'LC_CHEOPS_instTr1': '',
+                                            'LC_CHEOPS_instTr2': '',
+                                            'LC_CHEOPS_instTr3': '',
+                                            'LC_CHEOPS_instTr5': '',
+                                            },
+                       'model_definitions': {'Tr': {'model': 'batman'},
+                                             'common_parameters': {'P': True, 'tic': True, 'ecc_and_omega': True, 'inc': True}
+                                             }
+                       }
+                 }
