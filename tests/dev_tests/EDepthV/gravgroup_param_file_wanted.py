9efb11ddf65bfab08b111e821956e2ff66840e63
odemangeon <olivier.demangeon@astro.up.pt>
2022-09-21 15:14:26 +0100

    Updated wanted gravgroup_param_files for TTV and EDepthV

--- tests/dev_tests/EDepthV/gravgroup_param_file_wanted.py
+++ tests/dev_tests/EDepthV/gravgroup_param_file_wanted.py
@@ -2,13 +2,28 @@
 # -*- coding:  utf-8 -*-
 
 # Orbital models
-orbital_model = {'b': {'model4instrument': {'LC_CHEOPS_instTr0': '',
-                                            'LC_CHEOPS_instTr1': '',
-                                            'LC_CHEOPS_instTr2': '',
-                                            'LC_CHEOPS_instTr3': '',
-                                            'LC_CHEOPS_instTr5': '',
+orbital_model = {'b': {'model4instrument': {'LC_CHEOPS_inst1': '',
+                                            'LC_CHEOPS_inst2': '',
+                                            'LC_CHEOPS_inst3': '',
+                                            'LC_CHEOPS_inst4': '',
+                                            'LC_CHEOPS_inst5': '',
+                                            'LC_CHEOPS_inst6': '',
+                                            'LC_CHEOPS_inst7': '',
+                                            'LC_CHEOPS_inst8': '',
+                                            'LC_CHEOPS_inst9': '',
+                                            'LC_CHEOPS_inst10': '',
+                                            'LC_CHEOPS_inst11': '',
+                                            'LC_CHEOPS_inst12': '',
+                                            'LC_CHEOPS_inst13': '',
+                                            'LC_CHEOPS_inst14': '',
+                                            'LC_CHEOPS_inst15': '',
+                                            'LC_CHEOPS_inst16': '',
+                                            'LC_CHEOPS_inst17': '',
+                                            'LC_CHEOPS_inst18': '',
+                                            'LC_CHEOPS_inst19': '',
+                                            'LC_CHEOPS_inst20': '',
                                             },
-                       'model_definitions': {'Tr': {'model': 'batman'},
+                       'model_definitions': {'': {'model': 'batman'},
                                              'common_parameters': {'P': True, 'tic': True, 'ecc_and_omega': True, 'inc': True}
                                              }
                        }
--- tests/dev_tests/TTV/gravgroup_param_file_wanted.py
+++ tests/dev_tests/TTV/gravgroup_param_file_wanted.py
@@ -2,13 +2,17 @@
 # -*- coding:  utf-8 -*-
 
 # Orbital models
-orbital_model = {'b': {'model4instrument': {'LC_CHEOPS_instTr0': '',
-                                            'LC_CHEOPS_instTr1': '',
-                                            'LC_CHEOPS_instTr2': '',
-                                            'LC_CHEOPS_instTr3': '',
-                                            'LC_CHEOPS_instTr5': '',
+orbital_model = {'b': {'model4instrument': {'LC_CHEOPS_inst1': '1',
+                                            'LC_CHEOPS_inst2': '2',
+                                            'LC_CHEOPS_inst3': '3',
+                                            'LC_CHEOPS_inst4': '4',
+                                            'LC_CHEOPS_inst5': '5',
                                             },
-                       'model_definitions': {'Tr': {'model': 'batman'},
+                       'model_definitions': {'1': {'model': 'batman'},
+                                             '2': {'model': 'batman'},
+                                             '3': {'model': 'batman'},
+                                             '4': {'model': 'batman'},
+                                             '5': {'model': 'batman'},
                                              'common_parameters': {'P': True, 'tic': True, 'ecc_and_omega': True, 'inc': True}
                                              }
                        }
