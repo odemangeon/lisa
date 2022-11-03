2c5d99bf6a54ef24b4cb53512ccd8b9175a7b490
odemangeon <olivier.demangeon@astro.up.pt>
2022-11-03 11:35:33 +0000

    Bug fix related to inst_var correction in phase folded plot

--- lisa/explore_analyze/lc_plots.py
+++ lisa/explore_analyze/lc_plots.py
@@ -170,12 +170,12 @@ def create_LC_phasefolded_plots(fig, post_instance, df_fittedval, datasim_kwargs
     y_name = "$\Delta$F / F" if remove1 else "(F + $\Delta$F) / F"
     remove_dict_model = OrderedDict()
     for key, default in zip(["GP_model", "decorrelation", "inst_var", "contamination", "stellar_var", "1"],
-                            [True, True, remove_contamination, remove_contamination, True, remove1]
+                            [True, True, True, remove_contamination, True, remove1]
                             ):
         remove_dict_model[key] = default
     remove_dict_data = OrderedDict()
     for key, default in zip(["GP_model", "decorrelation_likelihood", "decorrelation", "inst_var", "contamination", "stellar_var", "1"],
-                            [True, True, True, remove_contamination, remove_contamination, True, remove1]
+                            [True, True, True, True, remove_contamination, True, remove1]
                             ):
         remove_dict_data[key] = default
     remove_dict_data_err = OrderedDict()
