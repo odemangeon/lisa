3f9dd59d91cc6feba06651b4717eda8374fd4c8e
odemangeon <olivier.demangeon@astro.up.pt>
2023-10-11 11:36:00 +0100

    Added ask4CreationDefaultFile function

--- lisa/tools/human_machine_interface/standard_questions.py
+++ lisa/tools/human_machine_interface/standard_questions.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
 """
 Module with functions allowing to ask standard questions
 """
 from loguru import logger
+from os.path import join
+
+from .QCM import QCM_utilisateur
 
 
 def Ask4Number(intitule_question, default_value=None):
@@ -45,3 +47,31 @@ def Ask4PositiveNumber(intitule_question, default_value=None):
                     logger.warning("Reply is not a POSITIVE number !")
             except ValueError:
                 logger.warning("Reply is not a float number !")
+
+def ask4CreationDefaultFile(path_file, default_file_content, default_folder=None):
+    """
+    """
+    answers_list_yn = ['y', 'n']
+    question = (f"File {path_file} doesn't exist. Do you want to create it ? {answers_list_yn}\n")
+    reply = QCM_utilisateur(question, answers_list_yn)
+    if reply == "y":
+
+        answers_list_create = ["absolute", "error"]
+        question = (f"File {path_file} doesn't exists. Do you want to\nCreate it at the 'absolute' "
+                    f"path: {path_file}"
+                    )
+        if default_folder is not None:
+            answers_list_create.append("run_folder")
+            run_folder_path = join(default_folder, path_file)
+            question += f"\nCreate it at the 'run_folder' path: {default_folder}"
+        question += (f"\nNot create it and raise an 'error' ? {answers_list_create}\n")
+        reply2 = QCM_utilisateur(question, answers_list_create)
+        if reply2 == 'absolute':
+            file_path = path_file
+        elif reply2 == "run_folder":
+            file_path = join(default_folder, path_file)
+        else:
+            raise ValueError(f"File {path_file} doesn't exist and the user doesn't want to create it.")
+        with open(file_path, 'x') as fdatasets:
+            fdatasets.write(default_file_content)
+        return file_path
