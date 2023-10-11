78c4f74be23c20779ee638ea0ee66850ed3ddb12
odemangeon <olivier.demangeon@astro.up.pt>
2023-10-11 11:37:33 +0100

    Update WASP-151 example

---
+++ examples/WASP-151/instrument_models.py
@@ -0,0 +1,6 @@
+# If you want to model one dataset of an instrument with a different instrument model from the others change 'inst' into whatever else you want (for example 'inst0')
+LC = {'EulerCam': {0: 'inst0', 1: 'inst1'},
+      'IAC80': {0: 'inst0', 1: 'inst1'},
+      'K2': {0: 'inst'},
+      'TRAPPIST': {0: 'inst'}}
+RV = {'CORALIE': {0: 'inst'}, 'SOPHIE': {0: 'inst'}}
