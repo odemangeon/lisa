635a63ddba7075bcf9b969f28b9a6390df0154bb
odemangeon <olivier.demangeon@astro.up.pt>
2023-10-11 11:37:33 +0100

    Update WASP-151 example

--- examples/WASP-151/script_mcmcexploration.py
+++ examples/WASP-151/script_mcmcexploration.py
@@ -1,56 +1,49 @@
-#!/usr/bin/python
-# -*- coding:  utf-8 -*-
 """
 Script template to perform an MCMC exploration.
 
 @TODO:
 """
-from logging import DEBUG, INFO
+from loguru import logger
 from math import ceil
 from os import getcwd
 from os.path import join
 
 from scipy.optimize import minimize
 from numpy import zeros_like
-from emcee import EnsembleSampler
 
 # If needed, add lisa folder to the python path here
-# import sys
-# lisa_folder = "../.."  # Change this if needed
+# lisa_folder = ".."  # Change this if needed
 # if lisa_folder not in sys.path:
 #     sys.path.append(lisa_folder)
 
 import lisa.posterior.core.posterior as cpost
 import lisa.emcee_tools.emcee_tools as et
-import lisa.tools.mylogger as ml
 from lisa.explore_analyze.misc import get_def_output_folders
 
 ###############################
 ## Definition of the parameters
 ###############################
-obj_name = "WASP-151"  # Name of you target star. Do not use "_"
-extension_exploration = "_initrun"  # extension of your exploration (will be added to the ouput files).
-model_category = "GravitionalGroups"  # Class of model: Can be GravitionalGroups, GravitionalGroupsDynamic
-nb_planet = 1  # Number of planets
-rv_model = "radvel"  # RV model: None will select the default model being radvel. Currently only radvel available
-transit_model = "batman"  # Transit model: None will select the default model being batman. Curretnly only batman available
-parametrisation = "Multis"  # Parametrisation to use. The possible choices depends on the model used. For GravitionalGroups it can be 'EXOFAST' or 'Multis'
-with_DeltaRV = True  # To you want to us an RV offset between RV instruments. More or less always True
-kwargs_post = {}  # Additional argument to pass to the posterior function. For example reference times.
+obj_name = "WASP-151"  # Change
 
-data_folder = join(getcwd(), "data")  # Folder where the data files are.
-run_folder = getcwd()  # Folder for the log and the parametrisation files
-output_folders = get_def_output_folders(run_folder=run_folder)  # Folder for the outputs
+extension_exploration = "_initrun"  # Change extension to add at the end (before .pk) of the name of the pickle files to save the exploration.
+model_category = "GravitionalGroups"
+nb_planet = 1
+kwargs_post = {}
+
+data_folder = join(getcwd(), "data")  # Change if needed: Folder where the data are located
+run_folder = getcwd()
+output_folders = get_def_output_folders(run_folder=run_folder)
 
 # Pre-minimisation parameters
-do_preminimization = True  # Performs a gradient base optimisation prior to the MCMC exploration
-N_maxiter_preminimization = 1000  # Maximum number of iteration for the gradient base optimisation
-xtol_preminimization = 1e-12  # Convergence criteria for the gradient base optimisation
+do_preminimization = True
+N_maxiter_preminimization = 1000
+xtol_preminimization = 1e-12
 
 # emcee parameters
-nwalker_fact = 2.5  # nwalkers will be round(nwalker_fact * nb parameters)
-nsteps_MCMC = 50000  # Number of steps for the MCMC exploration
-save_to_file = False  # Save the chains to a file while running.
+nwalker_fact = 4
+nsteps_MCMC = 50000
+check_convergence_every = 1000
+save_to_file = False
 cluster = False  # If you run this code on a cluster (not in ipython) change to True
 
 # Distribution for the choice of initial parameter values. For now you can only specify gaussian distributions
@@ -60,16 +53,16 @@ init_distrib = {}
 
 # If you already run a first MCMC and extracted fitted values, you can use them to draw the initial
 # values for a new MCMC run
-load_from_pickle = False  # If True the MCMC chains will start from the inferred parameter distribution found be the chain analysis specified below
-extension_analysis = ""  # Extension of the chain analysis that was used to derived the parameter values that you want to use as initial position distributions
+load_from_pickle = False
+extension_analysis = ""
 
 ##########################
 ## Execution of the script
 ##########################
 
 ## logger
-logger = ml.init_logger(with_ch=True, with_fh=True, logger_lvl=DEBUG, ch_lvl=INFO,
-                        fh_lvl=INFO, fh_file="{}.log".format(obj_name))
+if 'sinkid_file_explore' not in globals():
+    sinkid_file_explore = logger.add(join(output_folders['log'], 'exploration.log'), level='DEBUG')
 
 logger.info("########\nMCMC EXPLORATION")
 
@@ -85,23 +78,38 @@ post_instance.run_folder = run_folder
 logger.info("3. Add datasets from a datasets file.")
 post_instance.load_datasetsfile("datasets.txt")  # Change if needed by the name you gave or want to give to your dataset file.
 
-logger.info("4. Add a model")
-post_instance.define_model(category=model_category, name=obj_name, stars=1, planets=nb_planet,
-                           rv_model=rv_model, transit_model=transit_model)
+logger.info("4. Define the instrument models.")
+post_instance.load_instrumentmodelsfile("instrument_models.py")  # Change if needed by the name you gave or want to give to your dataset file.
 
-logger.info("5. Create inst_cat specific parameter file")
+logger.info("5. Define the noise models.")
+post_instance.load_noisemodelsfile("noise_models.py")  # Change if needed by the name you gave or want to give to your dataset file.
+
+logger.info("6. Add a model")
+post_instance.define_model(category=model_category, name=obj_name, stars=1, planets=nb_planet)
+
+logger.info("5. Create model specific parameter file")
 if cluster:
-    post_instance.model.create_instcat_paramfile(paramfile_path=None, answer_overwrite="n", answer_create=None)
+    post_instance.model.create_model_paramfile(paramfile=None, answer_overwrite="n", answer_create=None)
 else:
-    post_instance.model.create_instcat_paramfile(paramfile_path=None)  # paramfile_path=None the names are automatically chosen.
+    post_instance.model.create_model_paramfile(paramfile=None)  # paramfile=None the names are automatically chosen.
 
-    if len(post_instance.model.paramfile4instcat) > 0:
-        input("Modifiy the inst_cat specific paramerisation file: {}".format(post_instance.model.paramfile4instcat))
+    input("If there are a model specific paramerisation file please check it")
 
-logger.info("6. Load inst_cat specific parameter file")
+logger.info("6. Load model specific parameter file")
+post_instance.model.load_parameter_file_model()
+
+logger.info("7. Create inst_cat specific parameter file")
+if cluster:
+    post_instance.model.create_instcat_paramfiles(paramfile_path=None, answer_overwrite="n", answer_create=None)
+else:
+    post_instance.model.create_instcat_paramfiles(paramfile_path=None)  # paramfile_path=None the names are automatically chosen.
+
+    input("If there are any inst_cat specific paramerisation file please check them")
+
+logger.info("8. Load inst_cat specific parameter file")
 post_instance.model.load_instcat_paramfile()
 
-logger.info("7. Create noise model specific parameter file")
+logger.info("9. Create noise model specific parameter file")
 if cluster:
     post_instance.model.create_noisemodcat_paramfile(paramfile_path=None, answer_overwrite="n", answer_create=None)
 else:
@@ -110,13 +118,13 @@ else:
     if len(post_instance.model.paramfile4noisemodcat) > 0:
         input("Modifiy the noise model specific paramerisation file: {}".format(post_instance.model.paramfile4noisemodcat))
 
-logger.info("8. Load noise model category specific parameter file")
+logger.info("10. Load noise model category specific parameter file")
 post_instance.model.load_noisemodcat_paramfile()
 
-logger.info("9. Set parametrisation of the model")
-post_instance.model.set_parametrisation(parametrisation=parametrisation, with_DeltaRV=with_DeltaRV)
+logger.info("11. Set parametrisation of the model")
+post_instance.model.set_parametrisation()
 
-logger.info("10. Create and modify the paramerisation file")
+logger.info("12. Create and modify the paramerisation file")
 if cluster:
     post_instance.model.create_parameter_file("param_file.py", answer_overwrite="n", answer_create=None)
 else:
@@ -124,35 +132,33 @@ else:
 
     input("Modifiy the paramerisation file")
 
-logger.info("11. Load the paramerisation file")
+logger.info("13. Load the paramerisation file")
 post_instance.model.load_parameter_file()
 
-logger.info("12. Create datasimulator functions")
+logger.info("14. Create datasimulator functions")
 test = post_instance.get_datasimulators()
 
-logger.info("13. Create likelihood functions")
+logger.info("15. Create likelihood functions")
 post_instance.get_lnlikelihoods()
 
-logger.info("14. Create prior functions")
-post_instance.get_individal_lnpriors()
+logger.info("16. Create prior functions")
 post_instance.get_lnpriors()
 
-logger.info("15. Create posterior functions")
+logger.info("17. Create posterior functions")
 post_instance.get_lnposteriors()
-l_param_name = post_instance.lnposteriors.dataset_db["all"].arg_list["param"]
+l_param_name = post_instance.lnposteriors.dataset_db["all"].param_model_names_list
 
-logger.info("16. Save posterior instance")
+logger.info("18. Save posterior instance")
 post_instance.save_post_instance(pickle_folder=output_folders["pickles_explore"])
 
-logger.info("17. Create sampler")
-ndim = len(post_instance.lnposteriors.dataset_db["all"].arg_list["param"])
+logger.info("19. Create sampler")
+ndim = len(post_instance.lnposteriors.dataset_db["all"].param_model_names_list)
 lnpostfn = post_instance.lnposteriors.dataset_db["all"].function
-arg_list = post_instance.lnposteriors.dataset_db["all"].arg_list
 lnpriorfn = post_instance.lnpriors.dataset_db["all"].function
 lnlikefn = post_instance.lnlikelihoods.dataset_db["all"].function
 nwalkers = ceil(int(ndim * nwalker_fact) / 2) * 2  # To get an even number of walkers
 
-logger.info("18. Create initial value")
+logger.info("20. Create initial value")
 if load_from_pickle:
     if load_from_pickle:
         logger.info("0. Load from pickle")
@@ -170,7 +176,7 @@ p0 = et.generate_random_init_pos(nwalker=nwalkers, post_instance=post_instance,
                                  init_distrib=init_distrib_final)
 
 if not load_from_pickle and do_preminimization:
-    logger.info("19. AMOEBA minimization")
+    logger.info("21. AMOEBA minimization")
     p1 = zeros_like(p0)
 
     def lnpostfnminus(p):
@@ -183,10 +189,10 @@ if not load_from_pickle and do_preminimization:
 else:
     p1 = p0
 
-logger.info("19. Perform MCMC exploration")
+logger.info("22. Perform MCMC exploration")
 sampler = et.explore(nwalkers=nwalkers, ndim=ndim, log_prob_fn=lnpostfn, p0=p1, nsteps=nsteps_MCMC, kwargs_prob_fn=kwargs_post,
                      save_to_file=save_to_file, filename=f"{obj_name}_chain.h5", file_folder=output_folders["dats"],
-                     check_convergence_every=1000, ntau=100, tol=0.01, l_param_name=l_param_name)
+                     check_convergence_every=check_convergence_every, ntau=100, tol=0.01, l_param_name=l_param_name)
 et.save_emceesampler(sampler, l_param_name, obj_name, extension_exploration=extension_exploration, folder=output_folders["pickles_explore"])
 
 chain = sampler.chain
